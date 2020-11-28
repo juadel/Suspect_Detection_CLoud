363593ee0f29a37ea2dbc7565977a989a2531c35
juan <juadel@hotmail.com>
2020-11-28 12:59:03 -0800

    test

--- Backend/src/Business/settingsItems.ts
+++ Backend/src/Business/settingsItems.ts
@@ -33,6 +33,7 @@ export async function createCamSets( event: APIGatewayProxyEvent ): Promise<Sett
 export async function startStreaming(event: APIGatewayProxyEvent ): Promise<string> {
     const userId = getUserId(event);
     const cameraId = event.pathParameters.cameraid;
+    console.log(process.env.SERVER_ENDPOINT+'/start?userId='+userId+'&cameraId='+cameraId)
     const response :string = await axios.get(process.env.SERVER_ENDPOINT+'/start?userId='+userId+'&cameraId='+cameraId)
     return response
 

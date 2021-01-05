ab92b624b965bc58df0a7935db01f0312ffa6c94
juan <juadel@hotmail.com>
2021-01-04 21:15:25 -0800

    accordeon

--- home/src/App.js
+++ home/src/App.js
@@ -1,21 +1,29 @@
 
 import './App.css';
 import Typography from "@material-ui/core/Typography";
-import Main from './components/main'
+import ControlledAccordions from "./components/acordion";
+import Card from "@material-ui/core/Card"
+import Paper from "@material-ui/core/Paper"
 
 function App() {
   return (
-    <div className="App">
+    
+    <div className="container">
+    
       
-      
-        <div className="bg-text">
-        <Typography variant="h1" component="h2"   gutterBottom>
+        <div className="side">
+        <Typography  variant="h4" component="h2"   gutterBottom>
           Suspect Detector App 
         </Typography>
         </div>
-        <div className="useCases">
+        <div className="main">
+          <Paper>
+            <ControlledAccordions/>
+          </Paper>
+          
+        
         <Typography variant="h5" component="h2"> Use Cases: </Typography>
-        <Typography variant="body" color="inherit" component="p" align="justify">
+        <Typography variant="body"  component="p" align="justify">
                             
                             <li>For better detection and encoding results please use ID style images. </li>
                             <li>To crop an Image, use <a href="https://photoshop.adobe.com/" target="_blank" >Adobe Photoshop online</a> App.</li>
@@ -27,10 +35,8 @@ function App() {
                         
                         
          </Typography>
-         </div>
-         <div className="instructions">
-         
-        <Typography variant="h6"  component="p" align="justify">
+
+         <Typography variant="h6"  component="p" align="justify">
                             <ol>
                             <li> Register </li>
                             <li> Set up an Ip Camera </li>
@@ -39,13 +45,19 @@ function App() {
                             <li> You will recieve a Txt message on the number you used to register, notifying you when the suspect is detected by the camera </li>
                             <li> You can Stop/Start the streaming whenever it needs </li>
                             <li> Add multiple Suspects profiles and multiple Cameras </li>
-                            </ol>
-                        
-                        
+                            </ol>             
          </Typography>
+        
          </div>
+         <div>
+        <Typography  variant="h4" component="h2"   gutterBottom>
+            Suspect Detector App 
+          </Typography>
+        </div>
         
     </div>
+    
+  
   );
 }
 

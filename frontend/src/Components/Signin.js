fccd46f6438128d94ec1676e2cc0579b9650d35b
juan <juadel@hotmail.com>
2021-01-09 13:56:13 -0800

    adding validation

--- frontend/src/Components/Signin.js
+++ frontend/src/Components/Signin.js
@@ -9,6 +9,12 @@ import Card from "@material-ui/core/Card";
 import TextField from "@material-ui/core/TextField";
 import { Button } from "@material-ui/core";
 import styled from "styled-components";
+import Alert from "@material-ui/lab/Alert";
+import Modal from '@material-ui/core/Modal';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Container from '@material-ui/core/Container';
+import { Avatar, CardHeader } from "@material-ui/core";
 
 Amplify.configure(Cognito);
 const AUTH_USER_TOKEN_KEY = 'ReactAmplify.TokenKey';
@@ -60,14 +66,23 @@ const Register = styled.div`
     
     
 `;
+const RegModal = styled.div`
+    background-color : rgb(247, 239, 239);
+    color: rgb(255, 255, 255);
+    font-weight: bold;
 
-// amplify-authenticator {
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-//     flex: 1;
-//     height: 100vh;
-//   }
+    border-radius: 25px;
+    position: absolute;
+    
+
+    /* z-index: 2; */
+    width: 30%;
+    padding: 15px;
+    text-align: center;
+    
+`;
+
+
 
 Amplify.configure(Cognito);
 
@@ -77,7 +92,8 @@ class Signin extends Component {
     constructor(props){
         super(props);
         this.state={
-            username:"", password:"", loading: false
+            username:"", password:"", loading: false, newUsername: "", newPassword:"", setOpenRegisterModal: false, userConfirmed: null , 
+            setOpenConfirmModal: false, code: null
         }
     }
 
@@ -96,9 +112,34 @@ handleSignIn = () =>{
     this.signIn();
 }
 
+handleClose = ()=>{
+    this.setState({
+        setOpenRegisterModal: false,
+        setOpenConfirmModal : false
+    })
+}
+
+handleCreateAccount = ()=>{
+    this.setState({
+        setOpenRegisterModal: true
+    })
+}
+
+handleSubmitCreate =() =>{
+    this.signUp()
+}
+
+handleSubmitValidate =() =>{
+    this.validate()
+}
+
+handleResendCode =()=>{
+    this.reSendCode()
+}
+
 async  signIn() {
     this.setState({loading: true})
-    console.log(this.state.username)
+    //console.log(this.state.username)
         await Auth.signIn(this.state.username, this.state.password)
         .then(user => {
             console.log(this.props)
@@ -113,16 +154,77 @@ async  signIn() {
             console.log(user)
             
             window.location.pathname = "/api";
-
         })
         .catch(error => {
         console.log('error signing in', error);
-        this.setState({loading:false})
-        });
+        if (error.code==="NotAuthorizedException"){
+            alert(error.message)
+        }
+        else{
+            this.setState({
+                loading:false,
+                setOpenConfirmModal:true
+                });
+            alert("Please confirm you e-mail");
+        }
+    })
 }
 
+async signUp(){
+    let username = this.state.newUsername;
+    let password = this.state.newPassword;
+    let email = this.state.email;
+    let phone_number = this.state.phone_number;
+    let name = this.state.name;
+    await Auth.signUp({
+        username,
+        password,
+        attributes:{
+            email,
+            phone_number,
+            name
+        }
+
+    }).then(user =>{
+        this.setState({
+            userConfirmed: user.userConfirmed,
+            setOpenConfirmModal: true 
+        })
+    }).catch(error =>{
+        console.log('error signing in', error);
+        alert(error.message)}
+    ) 
+}
+
+async validate(){
+    console.log(this.state.username)
+    await Auth.confirmSignUp(this.state.username, this.state.code)
+    .then(user => {
+        this.setState({
+            userConfirmed: user.userConfirmed,
+            setOpenConfirmModal: false,
+            setOpenRegisterModal: false
+        })
+        alert("email validated, please log in")
+    })
+    .catch(error =>{
+        console.log('error Validating, error');
+        alert(error.message)}
+    )
+}
+
+async reSendCode(){
+    try{
+        await Auth.resendSignUp(this.state.username);
+        console.log('code resent successfully');
+    } catch (err){
+        console.log('error resending code: ', err);
+    }
+    
+}
 
 render(){
+    
     return(
         <div>
             <User>
@@ -140,12 +242,79 @@ render(){
                 <Button variant="contained"  onClick={this.handleSignIn} >Log In</Button>
             </ButtonStyled>
             <Register>
-            <Button size="small" >
+            <Button size="small" onClick={this.handleCreateAccount}>
                     Create an Account
             </Button>
            
             </Register>
-        
+            {/* // -------------------------------------- REGISTER FORM ---------------------------------------------// */}
+            
+            <Modal open={this.state.setOpenRegisterModal} onClose={this.handleClose} style={{display:'flex',alignItems:'center',justifyContent:'center'}}>
+            <RegModal>
+               
+                <Card>
+                    <CardHeader avatar={<Avatar>A</Avatar>} title="New Account"/>
+                   
+                <CardContent>
+                <Typography variant="body2" color="inherit" component="p" align="justify">
+                            
+                            <li>Password must be minimum 8 characters long.</li>
+                            <li>Password must have at least one special characters.</li>
+                            <li>Password must have at least one capital letter.</li>
+                            <li>Include the country code in Phone number.</li>
+                            <li>E.g., Phone: +12345678910</li>
+                            
+                        
+                        
+                </Typography>
+                <TextField required id="standard-required-5" label="Name"  onChange={this.handleFormInput} name="name"/>
+                <TextField required id="standard-required-1" label="Username"  onChange={this.handleFormInput} name="newUsername"/>
+                <TextField required id="standard-password-input-2" label="Password" type="password" onChange={this.handleFormInput} name="newPassword"/>
+                <TextField required id="standard-required-3" label="e-mail"  onChange={this.handleFormInput} name="email"/>
+                <TextField required id="standard-required-4" defaultValue="+1" label="Phone Number"  onChange={this.handleFormInput} name="phone_number"/>
+                
+                
+                </CardContent>
+                <CardActions>
+                    <Button variant="contained" size="small"  onClick={this.handleSubmitCreate}>
+                    Sign Up
+                    </Button>    
+                </CardActions>
+                </Card>
+                
+            </RegModal>
+            </Modal>
+            {/* // -------------------------------------- CONFIRM FORM ---------------------------------------------// */}
+            <Modal open={this.state.setOpenConfirmModal} onClose={this.handleClose} style={{display:'flex',alignItems:'center',justifyContent:'center'}}>
+            <RegModal>
+               
+                <Card>
+                    <CardHeader title="Confirm your Account"/>
+                   
+                <CardContent>
+                <Typography variant="body2" color="inherit" component="p" align="justify">
+                            
+                            <li>Please check your email.</li>
+                            <li>Validate the code.</li>        
+                </Typography>
+                <TextField required id="standard-required-5" label="Validation Code"  onChange={this.handleFormInput} name="code"/>
+                
+                
+                
+                </CardContent>
+                <CardActions>
+                    <Button variant="contained" size="small"  onClick={this.handleSubmitValidate}>
+                    Validate
+                    </Button>
+                    <Button variant="contained" size="small"  onClick={this.handleResendCode}>
+                    Re-send code
+                    </Button>        
+                </CardActions>
+                </Card>
+                
+            </RegModal>
+            </Modal>
+            
          </div>
         
     )

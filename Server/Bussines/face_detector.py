e45af69121424be2c51d697244ea1e7192230104
juan <juadel@hotmail.com>
2021-01-16 23:46:49 -0800

    snapshot

--- Server/Bussines/face_detector.py
+++ Server/Bussines/face_detector.py
@@ -65,8 +65,8 @@ class FaceDetectorProcess:
         self.suspectData.createEncodings()
         return
 
-    def snapShot(self, face_locations,frame, name ):
-        for top, right, bottom, left in face_locations:
+    def snapShot(self, face_locations,face_names, frame, name ):
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
               # Scale back up face locations since the frame we detected in was scaled to 1/4 size
             logging.warning("Snaphot")
             top *= 4
@@ -154,7 +154,7 @@ class FaceDetectorProcess:
                         face_locations = face_recognition.face_locations(rgb_small_frame)
                         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-                        #face_names = []
+                        face_names = []
                             
                         for face_encoding in face_encodings:
                                 # See if the face is a match for the known face(s)
@@ -172,24 +172,26 @@ class FaceDetectorProcess:
                                         log[name] = now
                                         logging.warning(f'{name} seen no more than 10 min ago')
                                     else:
+                                        face_names.append(name)
                                         logging.warning(f'{name} Return to site')
                                         log[name]=now
                                         date = now.strftime("%m/%d/%y, %H:%M")
-                                        self.snapShot(face_locations, frame, name)
+                                        self.snapShot(face_locations,face_names, frame, name)
                                         self.reportFinding(name, date)
 
                                 else:
                                         #logger(name, self.settings["phone"])
+                                    face_names.append(name)
                                     logging.warning(f'{name} seen')
                                     log[name] = now
                                     print(name)
                                     date = now.strftime("%m/%d/%y, %H:%M")
-                                    self.snapShot(face_locations, frame, name)
+                                    self.snapShot(face_locations,face_names, frame, name)
                                     self.reportFinding(name, date)
                                     
 
 
-                            #face_names.append(name)
+                            face_names.append(name)
                                 
                                 # if cv2.waitKey(1) & 0xFF == ord('q'):
                                 #     break
